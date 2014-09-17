fac8f4eca83887074ac3ceb6b5d1ccd825f2574b
Benoit Maggi <benoit.maggi@cea.fr>
2014-09-17 14:56:28 +0200

    Fix #11 Add header and license to Activator.java
    Signed-off-by: Benoit Maggi <benoit.maggi@cea.fr>

--- org.eclipse.papyrus.uml.configuration.simplified/src/org/eclipse/papyrus/uml/configuration/simplified/Activator.java
+++ org.eclipse.papyrus.uml.configuration.simplified/src/org/eclipse/papyrus/uml/configuration/simplified/Activator.java
@@ -1,3 +1,15 @@
+/*****************************************************************************
+ * Copyright (c) 2014 CEA LIST.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *
+ *****************************************************************************/
 package org.eclipse.papyrus.uml.configuration.simplified;
 
 import org.eclipse.jface.resource.ImageDescriptor;
